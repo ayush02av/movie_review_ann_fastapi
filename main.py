8db91a5e34ccea003e0fa09ba77fcb102731fbae
Ayush Verma <ayush02av@gmail.com>
2024-01-22 00:39:44 +0530

    Railway deploy ready

--- main.py
+++ main.py
@@ -52,4 +52,4 @@ async def predict_text(prompt: str):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
